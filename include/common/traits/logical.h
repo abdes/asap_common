13fe0b464ca6b1159c967a49d00fffaef6a13499
Abdessattar Sassi <abde.sassi@gmail.com>
2018-07-27 23:37:14 +0400

    added implementation of logical traits similar to c++17

---
+++ include/common/traits/logical.h
@@ -0,0 +1,46 @@
+//        Copyright The Authors 2018.
+//    Distributed under the 3-Clause BSD License.
+//    (See accompanying file LICENSE or copy at
+//   https://opensource.org/licenses/BSD-3-Clause)
+
+#pragma once
+
+#include <type_traits>
+
+namespace asap {
+
+template <class...>
+struct conjunction : std::true_type {};
+
+template <class B>
+struct conjunction<B> : B {};
+
+template <class B1, class... Bi>
+struct conjunction<B1, Bi...>
+    : std::conditional<B1::value, conjunction<Bi...>, B1>::type {};
+
+template <class...>
+struct disjunction : std::false_type {};
+
+template <class B>
+struct disjunction<B> : B {};
+
+template <class B1, class... Bi>
+struct disjunction<B1, Bi...>
+    : std::conditional<B1::value, B1, disjunction<Bi...> >::type {};
+
+template <class... Bi>
+constexpr bool conjunction_v = conjunction<Bi...>::value;
+
+template <class... Bi>
+constexpr bool disjunction_v = disjunction<Bi...>::value;
+
+template <bool B>
+struct bool_constant : std::integral_constant<bool, B> {};
+
+template <class B>
+struct negation : bool_constant<!bool(B::value)> {};
+template <class B>
+constexpr bool negation_v = negation<B>::value;
+
+}  // namespace asap
---
+++ test/traits_logical_test.cpp
@@ -0,0 +1,58 @@
+//        Copyright The Authors 2018.
+//    Distributed under the 3-Clause BSD License.
+//    (See accompanying file LICENSE or copy at
+//   https://opensource.org/licenses/BSD-3-Clause)
+
+#include <catch2/catch.hpp>
+
+#include <common/traits/logical.h>
+
+using asap::conjunction;
+using asap::disjunction;
+using asap::negation;
+
+TEST_CASE("Conjunction_typedef", "[common][traits][logical]") {
+  // Check for required typedefs
+  typedef conjunction<std::true_type, std::true_type> test_type;
+  typedef test_type::value_type value_type;
+  typedef test_type::type type;
+  typedef test_type::type::value_type type_value_type;
+  typedef test_type::type::type type_type;
+}
+
+TEST_CASE("Disjunction_typedef", "[common][traits][logical]") {
+  // Check for required typedefs
+  typedef disjunction<std::false_type, std::true_type> test_type;
+  typedef test_type::value_type value_type;
+  typedef test_type::type type;
+  typedef test_type::type::value_type type_value_type;
+  typedef test_type::type::type type_type;
+}
+
+TEST_CASE("Negation_typedef", "[common][traits][logical]") {
+  // Check for required typedefs
+  typedef negation<std::false_type> test_type;
+  typedef test_type::value_type value_type;
+  typedef test_type::type type;
+  typedef test_type::type::value_type type_value_type;
+  typedef test_type::type::type type_type;
+}
+
+TEST_CASE("Value", "[common][traits][logical]") {
+  REQUIRE(negation<std::false_type>{});
+  REQUIRE(!negation<std::true_type>{});
+  REQUIRE(conjunction<>{});
+  REQUIRE(!disjunction<>{});
+  REQUIRE(conjunction<std::true_type>{});
+  REQUIRE(!conjunction<std::false_type>{});
+  REQUIRE(disjunction<std::true_type>{});
+  REQUIRE(!disjunction<std::false_type>{});
+  REQUIRE(conjunction<std::true_type, std::true_type>{});
+  REQUIRE(!conjunction<std::true_type, std::false_type>{});
+  REQUIRE(disjunction<std::false_type, std::true_type>{});
+  REQUIRE(!disjunction<std::false_type, std::false_type>{});
+  REQUIRE(conjunction<std::true_type, std::true_type, std::true_type>{});
+  REQUIRE(!conjunction<std::true_type, std::true_type, std::false_type>{});
+  REQUIRE(disjunction<std::false_type, std::false_type, std::true_type>{});
+  REQUIRE(!disjunction<std::false_type, std::false_type, std::false_type>{});
+}
