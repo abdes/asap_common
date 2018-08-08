e2cd48ca49c633600700b3b23aeb1ded21d892eb
Abdessattar Sassi <abde.sassi@gmail.com>
2018-08-08 16:59:00 +0400

    improved the logging test cases

--- test/logging_test.cpp
+++ test/logging_test.cpp
@@ -6,44 +6,136 @@
 #include <catch2/catch.hpp>
 
 #include <common/logging.h>
+#include <spdlog/sinks/base_sink.h>
+#include <spdlog/details/null_mutex.h>
+
+#include <mutex>
+#include <sstream>
+#include <iostream>
+
+template<typename Mutex>
+class TestSink : public spdlog::sinks::base_sink<Mutex> {
+ public:
+
+  void Reset() {
+    out_.clear();
+    called_ = 0;
+  }
+
+  std::ostringstream out_;
+  int called_{0};
+
+ protected:
+  void _sink_it(const spdlog::details::log_msg &msg) override {
+    ++called_;
+    out_.write(msg.formatted.data(), msg.formatted.size());
+  }
+
+  void _flush() override {
+    out_.flush();
+  }
+};
+
+using TestSink_mt = TestSink<std::mutex>;
+using TestSink_st = TestSink<spdlog::details::null_mutex>;
 
 namespace asap {
 namespace logging {
 
 TEST_CASE("TestLoggable", "[common][logging]") {
+  auto *test_sink = new TestSink_mt();
+  auto test_sink_ptr = std::shared_ptr<spdlog::sinks::sink>(test_sink);
+  Registry::PushSink(test_sink_ptr);
+
   class Foo : Loggable<Id::TESTING> {
    public:
-    Foo() { ASLOG(trace, "Foo construcor"); }
+    Foo() { ASLOG(trace, "Foo constructor"); }
   };
 
-  ASLOG_MISC(info, "Hello World!");
   Foo foo;
+  REQUIRE(test_sink->called_);
+  auto msg = test_sink->out_.str();
+  REQUIRE(!msg.empty());
+  REQUIRE(msg.find("Foo constructor") != std::string::npos);
+
+  Registry::PopSink();
 }
 
 TEST_CASE("TestMultipleThreads", "[common][logging]") {
+  auto *test_sink = new TestSink_mt();
+  spdlog::set_pattern("%v");
+  auto test_sink_ptr = std::shared_ptr<spdlog::sinks::sink>(test_sink);
+  Registry::PushSink(test_sink_ptr);
+
   std::thread th1([]() {
     for (auto ii = 0; ii < 5; ++ii)
-      ASLOG_MISC(debug, "Logging from thread 1: {}", ii);
+      ASLOG_MISC(debug, "THREAD_1: {}", ii);
   });
   std::thread th2([]() {
     auto &test_logger = Registry::GetLogger(Id::TESTING);
     for (auto ii = 0; ii < 5; ++ii)
-      ASLOG_TO_LOGGER(test_logger, trace, "Logging from thread 2: {}", ii);
+      ASLOG_TO_LOGGER(test_logger, trace, "THREAD_2: {}", ii);
   });
   th1.join();
   th2.join();
+
+  REQUIRE(test_sink->called_ == 10);
+  std::istringstream msg_reader(test_sink->out_.str());
+  std::string line;
+  auto expected_seq_th1 = 0;
+  auto expected_seq_th2 = 0;
+  while (std::getline(msg_reader, line)) {
+    if (line.find("THREAD_1") != std::string::npos) {
+      REQUIRE(line.find(std::string("THREAD_1: ") + std::to_string(expected_seq_th1)) != std::string::npos);
+      ++expected_seq_th1;
+    }
+    if (line.find("THREAD_2") != std::string::npos) {
+      REQUIRE(line.find(std::string("THREAD_2: ") + std::to_string(expected_seq_th2)) != std::string::npos);
+      ++expected_seq_th2;
+    }
+  }
+  REQUIRE(expected_seq_th1 == 5);
+  REQUIRE(expected_seq_th2 == 5);
+
+  Registry::PopSink();
 }
 
 TEST_CASE("TestLogWithPrefix", "[common][logging]") {
+  auto *test_sink = new TestSink_mt();
+  auto test_sink_ptr = std::shared_ptr<spdlog::sinks::sink>(test_sink);
+  Registry::PushSink(test_sink_ptr);
+  
   auto &test_logger = Registry::GetLogger(Id::TESTING);
 
   AS_DO_LOG(test_logger, debug, "message");
+  REQUIRE(test_sink->called_ == 1);
+  REQUIRE(test_sink->out_.str().find("message") != std::string::npos);
+  test_sink->Reset();
+
   AS_DO_LOG(test_logger, debug, "message {}", 1);
+  REQUIRE(test_sink->called_ == 1);
+  REQUIRE(test_sink->out_.str().find("message 1") != std::string::npos);
+  test_sink->Reset();
+
   AS_DO_LOG(test_logger, debug, "message {} {}", 1, 2);
+  REQUIRE(test_sink->called_ == 1);
+  REQUIRE(test_sink->out_.str().find("message 1 2") != std::string::npos);
+  test_sink->Reset();
+
   AS_DO_LOG(test_logger, debug, "message {} {} {}", 1, 2, 3);
-  AS_DO_LOG(test_logger, debug, "message {} {} {} {}", 1, 3, 3, 4);
+  REQUIRE(test_sink->called_ == 1);
+  REQUIRE(test_sink->out_.str().find("message 1 2 3") != std::string::npos);
+  test_sink->Reset();
+
+  AS_DO_LOG(test_logger, debug, "message {} {} {} {}", 1, 2, 3, 4);
+  REQUIRE(test_sink->called_ == 1);
+  REQUIRE(test_sink->out_.str().find("message 1 2 3 4") != std::string::npos);
+  test_sink->Reset();
+
+  Registry::PopSink();
 }
 
+namespace {
 class MockSink : public spdlog::sinks::sink {
  public:
   void log(const spdlog::details::log_msg &) override { ++called_; }
@@ -52,6 +144,7 @@ class MockSink : public spdlog::sinks::sink {
 
   int called_{0};
 };
+}
 
 TEST_CASE("TestLogPushSink", "[common][logging]") {
   auto *first_mock = new MockSink();
@@ -84,12 +177,16 @@ TEST_CASE("TestLogPushSink", "[common][logging]") {
   second_mock->Reset();
 
   Registry::PopSink();
+  // mute the logger output
+  auto *test_sink = new TestSink_mt();
+  auto test_sink_ptr = std::shared_ptr<spdlog::sinks::sink>(test_sink);
+  Registry::PushSink(test_sink_ptr);
   ASLOG_TO_LOGGER(test_logger, debug, "message");
 
   REQUIRE(first_mock->called_ == 0);
   REQUIRE(second_mock->called_ == 0);
-  first_mock->Reset();
-  second_mock->Reset();
+
+  Registry::PopSink();
 }
 
 }  // namespace logging
